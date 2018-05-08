f6f38e2218a32460578730398198700c9f1e855c
Evandro Junior <evandrojr@gmail.com>
2018-05-07 22:44:48 -0300

    Version changed

--- spec/awesome_print_spec.cr
+++ spec/awesome_print_spec.cr
@@ -1,9 +1,63 @@
 require "./spec_helper"
+require "ncurses"
 
 describe AwesomePrint do
-  it "lists contents of an array" do
+
+  it "Print a integer" do
+    a = 100
+    r = ap a
+    r.should eq 100
+  end
+
+  it "Print a string" do
+    a = "String"
+    r = ap a
+    r.should eq "\"String\""
+  end
+
+  it "lists contents of an array of Intergers" do
     a = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115]
     r = ap a
-    r.should eq "[100, 101, 102, 103, 104, 105, 106, \n107, 108, 109, 110, 111, 112, 113, \n114, 115]"
+    r.should eq "[100, 101, 102, 103, 104, 105, 106, \n 107, 108, 109, 110, 111, 112, 113, \n 114, 115]"
   end
+
+  it "lists contents of an array of Strings" do
+    a = ["100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115"]
+    r = ap a
+    r.should eq "[\"100\", \"101\", \"102\", \"103\", \"104\", \"105\", \"106\", \n \"107\", \"108\", \"109\", \"110\", \"111\", \"112\", \"113\", \n \"114\", \"115\"]"
+  end
+
+  it "Print a float" do
+    a = 100.5/3
+    r = ap a
+    r.should eq 33.5
+  end
+
+  it "Print a range" do
+    a = 3..5
+    r = ap a
+    r.should eq 3..5
+  end
+
+  it "Print a char" do
+    a = 'a'
+    r = ap a
+    r.should eq "'a'"
+  end
+
+  it "Print a hash" do
+    a = {"one" => 1, "two" => 2}
+    r = ap a
+    r.should eq a
+  end
+
+  it "Print a hash" do
+    a = [{"one" => 1, "two" => 2}, {"one" => 1, "two" => 2}] 
+    r = ap a
+    r.should eq "[{\"one\" => 1, \"two\" => 2}, {\"one\" => 1, \"two\" => 2}]"
+  end
+
+  
+  
+
 end
--- src/awesome_print.cr
+++ src/awesome_print.cr
@@ -1,30 +1,58 @@
 require "./awesome_print/*"
 
+
 def self.ap(v)
-  if v.is_a? Array
-    return Formater.array_formater(v)
-  end
-  puts v.inspect
+  r = Formater.selector(v)
+  print r
+  r
 end
 
+
 class Formater
+  
   MAX_ELEMENTS_PER_ROW = 7
 
-  def self.array_formater(vars)
+  def self.selector(v)
+    if v.is_a? Array
+      r = Formater.array(v)
+    elsif v.is_a? String
+      r = Formater.string(v)
+    elsif v.is_a? Char
+      r = Formater.char(v)
+    else
+      r = v
+    end  
+    return r  
+  end  
+
+  def self.integer(v)
+    return v
+  end  
+
+  def self.string(v)
+    return "\"#{v}\""
+  end  
+
+  def self.char(v)
+    return "'#{v}'"
+  end  
+
+
+  def self.array(vars)
     i = 0
     s = "["
     vars.each { |v|
-      s = s + v.to_s
+      s = s + selector(v).to_s
       i = i + 1
       if i < vars.size
         s = s + ", "
       end
       if i % MAX_ELEMENTS_PER_ROW == 0 && i < vars.size
-        s += "\n"
+        s += "\n "
       end
     }
     s += "]"
-    puts s
-    s
+    return s
+
   end
 end
