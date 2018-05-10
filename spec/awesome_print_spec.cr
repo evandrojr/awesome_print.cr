5d454481bbc296467c64e79199177995354abf26
Evandro Junior <evandrojr@gmail.com>
2018-05-10 05:14:58 -0300

    added benchmark image

--- spec/awesome_print_spec.cr
+++ spec/awesome_print_spec.cr
@@ -1,6 +1,7 @@
 require "./spec_helper"
 
-COLORS = %i(black red green)
+SYMBOLS  = %i(black red green)
+INTEGERS = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115]
 
 describe AwesomePrint do
   Spec.before_each {
@@ -23,8 +24,7 @@ describe AwesomePrint do
   end
 
   it "lists contents of an array of Intergers" do
-    a = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115]
-    r = ap a
+    r = ap INTEGERS
     r.should eq "[100, 101, 102, 103, 104, 105, 106, \n 107, 108, 109, 110, 111, 112, 113, \n 114, 115]"
   end
 
@@ -66,17 +66,20 @@ describe AwesomePrint do
 
   it "print colored" do
     Formater.coloring = true
-    data_types = ["string color", 1, 2, 3, 4, 'Z', 1.437643, 133.5e10] + COLORS
-
+    data_types = ["String color is green", "Char is yellow", 'a', 'Z', 1, 2, 3, 4, 1.437643, 133.5e10]
+    data_types = data_types + SYMBOLS + INTEGERS
+    puts "\n\n"
     puts "Benchmark:"
     puts
     puts "awesome_print's ap vs .inspect"
     puts
-    puts "data_types.inspect:"
+    puts "Boring data_types.inspect:"
+    puts
     puts data_types.inspect
     puts "(No colors nor column size limits)"
     puts
-    puts "ap data_types"
+    puts "Check it out ap data_types:"
+    puts
     ap data_types
     puts "(That is what I call an awesome print!)"
   end
